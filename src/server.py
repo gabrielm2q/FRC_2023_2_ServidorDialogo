0c1c9b5b896d5948e63332d913b0da8c85729cbc
Thalisson-Alves <thalisson2030@gmail.com>
2023-12-17 10:33:45 -0300

    Add simple webcam streaming and chat functionality

--- src/server.py
+++ src/server.py
@@ -1,66 +1,19 @@
-import socket, cv2
-import numpy as np
-import threading as thread
-
-# tcp and ipv4 address family
-tcp = socket.SOCK_STREAM
-afm = socket.AF_INET
-
-# user a
-usera_ip = socket.gethostbyname(socket.gethostname())
-usera_port = 2000
-
-# creating socket
-sa = socket.socket(afm, tcp)
-sb = socket.socket(afm, tcp)
-
-# Binding ports
-sa.bind((usera_ip, usera_port))
-
-# listening port and creating session
-sa.listen()
-session, addr = sa.accept()
-
-print(addr)
-
-# connecting to userb
-sb.connect((usera_ip, 2001))
+import asyncio
+import websockets
 
 
-def receive():
+async def handler(websocket: websockets.WebSocketServerProtocol):
     while True:
-        en_photo = session.recv(921600)
-        image_arr = np.frombuffer(en_photo, np.uint8)
-        image = cv2.imdecode(image_arr, cv2.IMREAD_COLOR)
-        if type(image) is type(None):
-            pass
-        else:
-            cv2.imshow("Video stream", image)
-            if cv2.waitKey(10) == 13:
-                break
-
-    cv2.destroyAllWindows()
-    exit()
+        message = await websocket.recv()
+        print(message)
 
 
-def send():
-    capture = cv2.VideoCapture(0)
-
-    while True:
-        ret, photo = capture.read()
-        if ret == True:
-            en_photo = cv2.imencode(".jpg", photo)[1].tobytes()
-            sb.sendall(en_photo)
-        else:
-            pass
-
-    exit()
+async def main():
+    host, port = "localhost", 8765
+    print(f"Listening on ws://{host}:{port}")
+    async with websockets.serve(handler, host, port):
+        await asyncio.Future()
 
 
-# send and receive threads
-send_thread = thread.Thread(target=send)
-recv_thread = thread.Thread(target=receive)
-
-# starting threads
-send_thread.start()
-recv_thread.start()
+if __name__ == "__main__":
+    asyncio.run(main())
