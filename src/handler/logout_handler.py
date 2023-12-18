5595da9b657f65c254895be1eb2e4af843a967a0
Thalisson-Alves <thalisson2030@gmail.com>
2023-12-18 17:28:17 -0300

    Refactor logout_handler.py to send chat notifications when a user logs out

--- src/handler/logout_handler.py
+++ src/handler/logout_handler.py
@@ -1,14 +1,18 @@
-from model.user_state import UserStateRepository
+import json
+
+from model.user_state import AllUserInfos
 from handler import HandlerResponse
 
 
-async def logout_handler(remote_address: tuple[str, int]) -> HandlerResponse:
-    UserStateRepository.set_offline(remote_address)
-
-    return HandlerResponse(
-        {
-            "success": True,
-            "message": "Logout efetuado com sucesso",
-            "data": None,
-        }
-    )
+async def logout_handler(remote_address: tuple[str, int]) -> None:
+    user = AllUserInfos.get_user(remote_address)
+    if user is None:
+        return
+    for info in AllUserInfos.get_other_participants(user):
+        response = HandlerResponse(
+            type="chat",
+            success=True,
+            message=f"{user.nickname} saiu da chamada",
+        )
+        await info.websocket.send(json.dumps(response))
+    AllUserInfos.remove_user(remote_address)
