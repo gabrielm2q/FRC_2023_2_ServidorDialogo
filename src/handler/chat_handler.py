6d96b41f469061e8a8d8e301d47e65e15b7cf489
Thalisson-Alves <thalisson2030@gmail.com>
2023-12-18 17:27:54 -0300

    Add chat_handler.py for handling chat functionality

---
+++ src/handler/chat_handler.py
@@ -0,0 +1,46 @@
+import json
+from typing import TypedDict
+
+from model.user_state import AllUserInfos
+from handler import HandlerResponse
+
+from websockets import WebSocketServerProtocol
+
+
+class ChatData(TypedDict):
+    type: str
+    remote_address: tuple[str, int]
+    message: str
+    websocket: WebSocketServerProtocol
+
+
+async def chat_handler(data: ChatData) -> None:
+    user = AllUserInfos.get_user(data["remote_address"])
+    if user is None:
+        response = HandlerResponse(
+            type=data["type"], success=False, message="Usuário não está logado"
+        )
+        return await data["websocket"].send(json.dumps(response))
+
+    if user.topic is None:
+        response = HandlerResponse(
+            type=data["type"],
+            success=False,
+            message="Usuário não está em nenhum tópico",
+        )
+        return await data["websocket"].send(json.dumps(response))
+
+    for info in AllUserInfos.get_other_participants(user):
+        response = HandlerResponse(
+            type=data["type"],
+            success=True,
+            message=f"{user.nickname}: {data['message']}",
+        )
+        await info.websocket.send(json.dumps(response))
+
+    response = HandlerResponse(
+        type=data["type"],
+        success=True,
+        message=f"Você: {data['message']}",
+    )
+    await user.websocket.send(json.dumps(response))
